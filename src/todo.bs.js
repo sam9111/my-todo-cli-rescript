5fd44bbb39e163f78a11aecbd17a2a2ca4a31a85
sam9111 <samyuktha19095@cse.ssn.edu.in>
2021-02-22 20:45:45 +0530

    All tests passed!

--- src/todo.bs.js
+++ src/todo.bs.js
@@ -4,6 +4,8 @@
 var Fs = require("fs");
 var Os = require("os");
 var Curry = require("bs-platform/lib/js/curry.js");
+var Belt_Int = require("bs-platform/lib/js/belt_Int.js");
+var Belt_Array = require("bs-platform/lib/js/belt_Array.js");
 
 var getToday = (function() {
   let date = new Date();
@@ -14,18 +16,204 @@ var getToday = (function() {
 
 var encoding = "utf8";
 
-console.log("Hello! today is " + Curry._1(getToday, undefined));
+function readFromfile(filename) {
+  if (!Fs.existsSync(filename)) {
+    return [];
+  }
+  var read_string = Fs.readFileSync(filename, {
+          encoding: encoding,
+          flag: "r"
+        }).trim();
+  return read_string.split(Os.EOL);
+}
 
-if (Fs.existsSync("todo.txt")) {
-  console.log("Todo file exists.");
-} else {
-  Fs.writeFileSync("todo.txt", "This is todo!" + Os.EOL, {
+function writeTofile(text, filename) {
+  var new_contents = Belt_Array.reduce(text, "", (function (lines, line) {
+          return lines + line + Os.EOL;
+        }));
+  Fs.writeFileSync(filename, new_contents, {
         encoding: encoding,
         flag: "w"
       });
-  console.log("Todo file created.");
+  
 }
+
+function appendTofile(line, filename) {
+  var line$1 = line + Os.EOL;
+  Fs.appendFileSync(filename, line$1, {
+        encoding: encoding,
+        flag: "a"
+      });
+  
+}
+
+function help(param) {
+  console.log("Usage :-\n$ ./todo add \"todo item\"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics");
+  
+}
+
+function add(todos) {
+  var len = todos.length;
+  if (len !== 1) {
+    if (len === 0) {
+      console.log("Error: Missing todo string. Nothing added!");
+      return ;
+    }
+    
+  } else {
+    var match = todos[0];
+    if (match === "") {
+      console.log("Error: Missing todo string. Nothing added!");
+      return ;
+    }
+    
+  }
+  return Belt_Array.forEach(todos, (function (item) {
+                appendTofile(item, "todo.txt");
+                console.log("Added todo: \"" + item + "\"");
+                
+              }));
+}
+
+function ls(param) {
+  var todos = readFromfile("todo.txt");
+  var len = todos.length;
+  if (len !== 1) {
+    if (len === 0) {
+      console.log("There are no pending todos!");
+      return ;
+    }
+    
+  } else {
+    var match = todos[0];
+    if (match === "") {
+      console.log("There are no pending todos!");
+      return ;
+    }
+    
+  }
+  var formatted_todos = Belt_Array.mapWithIndex(todos, (function (i, todo) {
+          return "[" + String(i + 1 | 0) + "] " + todo;
+        }));
+  return Belt_Array.forEach(Belt_Array.reverse(formatted_todos), (function (todo) {
+                console.log(todo);
+                
+              }));
+}
+
+function getTodo(todoNumber) {
+  var todos = readFromfile("todo.txt");
+  var index = Belt_Int.fromString(todoNumber);
+  if (index !== undefined) {
+    return Belt_Array.get(todos, index - 1 | 0);
+  }
+  
+}
+
+function removeTodo(todoToremove) {
+  var todos = readFromfile("todo.txt");
+  var updated_content = todos.filter(function (todo) {
+        return todo !== todoToremove;
+      });
+  return writeTofile(updated_content, "todo.txt");
+}
+
+function delTodo(todoNumber) {
+  var todoTodelete = getTodo(todoNumber);
+  if (todoTodelete !== undefined) {
+    removeTodo(todoTodelete);
+    console.log("Deleted todo #" + todoNumber);
+  } else {
+    console.log("Error: todo #" + todoNumber + " does not exist. Nothing deleted.");
+  }
+  
+}
+
+function del(todoNumber) {
+  if (todoNumber.length !== 0) {
+    return Belt_Array.forEach(todoNumber, delTodo);
+  } else {
+    console.log("Error: Missing NUMBER for deleting todo.");
+    return ;
+  }
+}
+
+function done_cmd(todoNumber) {
+  if (todoNumber !== undefined) {
+    var todoTomark = getTodo(todoNumber);
+    if (todoTomark !== undefined) {
+      removeTodo(todoTomark);
+      var itemDone = "x " + Curry._1(getToday, undefined) + " " + todoTomark;
+      appendTofile(itemDone, "done.txt");
+      console.log("Marked todo #" + todoNumber + " as done.");
+      return ;
+    }
+    console.log("Error: todo #" + todoNumber + " does not exist.");
+    return ;
+  }
+  console.log("Error: Missing NUMBER for marking todo as done.");
+  
+}
+
+function report(param) {
+  var pending = readFromfile("todo.txt").length;
+  var completed = readFromfile("done.txt").length;
+  console.log(Curry._1(getToday, undefined) + " Pending : " + String(pending) + " Completed : " + String(completed));
+  
+}
+
+var $$arguments = Belt_Array.map((process.argv.slice(2)), (function (argument) {
+        return argument.trim();
+      }));
+
+var command = Belt_Array.get($$arguments, 0);
+
+var inputs = Belt_Array.slice($$arguments, 1, $$arguments.length);
+
+function command_switch(command, inputs) {
+  if (command !== undefined) {
+    switch (command) {
+      case "add" :
+          return add(inputs);
+      case "del" :
+          return del(inputs);
+      case "done" :
+          return done_cmd(Belt_Array.get(inputs, 0));
+      case "help" :
+          console.log("Usage :-\n$ ./todo add \"todo item\"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics");
+          return ;
+      case "ls" :
+          return ls(undefined);
+      case "report" :
+          return report(undefined);
+      default:
+        console.log("Usage :-\n$ ./todo add \"todo item\"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics");
+        return ;
+    }
+  } else {
+    console.log("Usage :-\n$ ./todo add \"todo item\"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics");
+    return ;
+  }
+}
+
+command_switch(command, inputs);
 
 exports.getToday = getToday;
 exports.encoding = encoding;
-/*  Not a pure module */
+exports.readFromfile = readFromfile;
+exports.writeTofile = writeTofile;
+exports.appendTofile = appendTofile;
+exports.help = help;
+exports.add = add;
+exports.ls = ls;
+exports.getTodo = getTodo;
+exports.removeTodo = removeTodo;
+exports.delTodo = delTodo;
+exports.del = del;
+exports.done_cmd = done_cmd;
+exports.report = report;
+exports.$$arguments = $$arguments;
+exports.command = command;
+exports.inputs = inputs;
+exports.command_switch = command_switch;
+/* arguments Not a pure module */
--- src/todo.res
+++ src/todo.res
@@ -34,16 +34,173 @@ external writeFileSync: (string, string, fsConfig) => unit = "writeFileSync"
 
 let encoding = "utf8"
 
-/*
-NOTE: The code below is provided just to show you how to use the
-date and file functions defined above. Remove it to begin your implementation.
-*/
 
-Js.log("Hello Samyuktha! today is " ++ getToday())
 
-if existsSync("todo.txt") {
-  Js.log("Todo file exists.")
-} else {
-  writeFileSync("todo.txt", "This is todo!" ++ eol, {encoding: encoding, flag: "w"})
-  Js.log("Todo file created.")
+
+//reading file contents
+
+let readFromfile: string => array<string> = filename =>{
+  if !existsSync(filename){
+    []
+  }
+  else{
+    let read_string: string= Js.String.trim(
+      readFileSync(filename,{encoding: encoding,flag: "r"}))
+    Js.String.split(eol,read_string)
+  }
 }
+
+//truncating and overwriting file contents
+
+let writeTofile: (array<string>,string) => unit = (text,filename) =>{
+  let new_contents: string=text->Belt.Array.reduce("",
+    (lines,line) =>lines++line++eol)
+  writeFileSync(filename,new_contents,{encoding: encoding,flag: "w"})
+}
+
+//appending to file contents
+
+let appendTofile: (string,string) => unit = (line,filename) =>{
+  let line=line++eol
+  appendFileSync(filename,line,{encoding: encoding,flag: "a"})
+}
+
+//Help
+
+let help: unit => unit=()=>{
+Js.log("Usage :-
+$ ./todo add \"todo item\"  # Add a new todo
+$ ./todo ls               # Show remaining todos
+$ ./todo del NUMBER       # Delete a todo
+$ ./todo done NUMBER      # Complete a todo
+$ ./todo help             # Show usage
+$ ./todo report           # Statistics")
+}
+
+//Add new single or multiple todos
+
+let add: array<string> => unit= todos =>{
+  switch todos {
+    |[]|[""] => Js.log("Error: Missing todo string. Nothing added!")
+    |items =>{
+      items->
+      Belt.Array.forEach(
+        item=>{
+          appendTofile(item,"todo.txt")
+          Js.log(`Added todo: "${item}"`)
+        }
+      )
+    }
+  }
+}
+
+//List all pending todos
+
+let ls: unit => unit=()=>{
+  let todos: array<string> =readFromfile("todo.txt")
+  switch todos{
+    |[]|[""] => Js.log("There are no pending todos!")
+    |someTodos =>{
+      let formatted_todos: array<string> =someTodos->
+        Belt.Array.mapWithIndex(
+          (i,todo)=>{
+            `[${Belt.Int.toString(i+1)}] ${todo}`
+          })
+
+      Belt.Array.forEach(
+        Belt.Array.reverse(formatted_todos),
+          todo=> Js.log(todo)
+      )
+    }
+  }
+}
+
+//get todo from "todo.txt"
+
+let getTodo: string => option<string> =todoNumber=>{
+   let todos: array<string> =readFromfile("todo.txt")
+   let index: option<int> = Belt.Int.fromString(todoNumber)
+   switch index{
+    |None => None
+    |Some(i) =>Belt.Array.get(todos, i-1)
+    }
+}
+//remove todo from "todo.txt"
+
+let removeTodo: string => unit =todoToremove=>{
+  let todos: array<string> =readFromfile("todo.txt")
+  let updated_content: array<string> =Js.Array.filter(todo=>todo!=todoToremove,todos)
+  writeTofile(updated_content,"todo.txt")
+}
+
+//Delete single todo from "todo.txt"
+
+let delTodo: string => unit=todoNumber=>{
+  let todoTodelete: option<string> =getTodo(todoNumber)
+  switch todoTodelete{
+    |None => Js.log(`Error: todo #${todoNumber} does not exist. Nothing deleted.`)
+    |Some(deleted_todo)=>{
+      removeTodo(deleted_todo)
+      Js.log(`Deleted todo #${todoNumber}`)
+    }
+  }
+}
+//Delete todos from "todo.txt"
+let del: array<string> => unit=todoNumber=>{
+  switch todoNumber{
+    |[] => Js.log("Error: Missing NUMBER for deleting todo.")
+    |someNumbers =>{
+      someNumbers->Belt.Array.forEach(
+        number=>delTodo(number)
+      )
+    }
+  }
+}
+
+//Mark a todo item as completed
+
+let done_cmd: option<string> => unit=todoNumber=>{
+  switch todoNumber{
+    |None => Js.log(`Error: Missing NUMBER for marking todo as done.`)
+    |Some(number)=>{
+      let todoTomark: option<string>=getTodo(number)
+      switch todoTomark{
+          |None => Js.log(`Error: todo #${number} does not exist.`)
+          |Some(marked_todo)=>{
+            removeTodo(marked_todo)
+            let itemDone: string=`x ${getToday()} ${marked_todo}`
+            appendTofile(itemDone,"done.txt")
+            Js.log(`Marked todo #${number} as done.`)
+          }
+      }
+    }
+  }
+}
+
+//Generate report
+let report: unit => unit=()=>{
+  let pending: int=Belt.Array.length(readFromfile("todo.txt"))
+  let completed: int=Belt.Array.length(readFromfile("done.txt"))
+  Js.log(
+    `${getToday()} Pending : ${Belt.Int.toString(pending)} Completed : ${Belt.Int.toString(completed)}`
+  )
+}
+
+let arguments: array<string> =%raw(`process.argv.slice(2)`)->Belt.Array.map(
+  argument=>Js.String.trim(argument)
+)
+let command: option<string> =Belt.Array.get(arguments,0)
+let inputs: array<string> =Belt.Array.slice(arguments,~offset=1,~len=Belt.Array.length(arguments))
+let command_switch: (option<string>,array<string>) => unit = (command,inputs)=> {
+  switch command {
+    |Some("add")=>add(inputs)
+    |Some("ls")=>ls()
+    |Some("del")=>del(inputs)
+    |Some("done")=>done_cmd(Belt.Array.get(inputs,0))
+    |Some("report")=>report()
+    |Some("help")
+    |Some(_)
+    |None=> help()
+  }
+}
+command_switch(command,inputs)
